f61026e257f6efdebd448786876ed6a0d8894b71
Akashdeep <akash.kashhyap@gmail.com>
2023-08-15 09:52:58 +0530

    blog component issue

--- components/Blog.js
+++ components/Blog.js
@@ -10,7 +10,7 @@ const Blog = ({ blok }) => {
     <div className="blog-page" {...storyblokEditable(blok)}>
       <article>
         {blok?.blogAlert &&
-          blok?.blogAlert.map((nestedBlok) => (
+          blok?.blogAlert?.map((nestedBlok) => (
             <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
           ))}
         <h1 className="text-4xl tracking-tight font-semibold mb-10">
@@ -19,17 +19,14 @@ const Blog = ({ blok }) => {
         <div className="relative h-[415px] mb-10">
           <BannerImage blok={blok} />
         </div>
-        {blok?.body.map((nestedBlok) => (
+        {blok?.body?.map((nestedBlok) => (
           <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
         ))}
       </article>
       <aside>
-        {blok?.aside.map((nestedBlok) => (
+        {blok?.aside?.map((nestedBlok) => (
           <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
         ))}
-        {/* <div className="bg-slate-600 sticky top-10">
-         content
-       </div> */}
       </aside>
     </div>
   );
