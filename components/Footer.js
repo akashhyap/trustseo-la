18061873e071382dd55767bdaa0100035408ba2d
Akashdeep <akash.kashhyap@gmail.com>
2023-08-17 18:21:57 +0530

    adding footer menu

--- components/Footer.js
+++ components/Footer.js
@@ -9,6 +9,7 @@ import {
 } from "react-icons/bi";
 
 const Footer = ({ config }) => {
+  console.log("footer config", config);
   return (
     <div className="bg-gray-200 py-8">
       <div className="max-w-6xl mx-auto flex flex-wrap justify-between items-center">
